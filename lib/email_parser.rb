4b42eb624f5b5bb722824d139fcf63cc118301e0
Indu Ramesh <induramesh@gmail.com>
2019-09-09 14:54:21 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -8,12 +8,12 @@ class EmailAddressParser
   
   attr_accessor :email_list
  
-  def initialize(email)
-    @email_list = email_list
+  def initialize(emails)
+    @email_list = emails
   end
 
   def parse
-    email_list.delete(',').split.uniq
+    email_list.split(/,| /).uniq
   end
   
 end 
